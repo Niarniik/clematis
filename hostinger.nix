5f6752231bf6bbabb0bbed3f41fc01dd366a035c
Julius Kreutz <julius@kreutz.dev>
2025-04-03 15:51:36 +0200

    fix: cloud-init

--- hostinger.nix
+++ hostinger.nix
@@ -9,6 +9,20 @@
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
+  # FIXME: TEMP
+  # https://github.com/NixOS/nixpkgs/blob/nixpkgs-unstable/pkgs/top-level/all-packages.nix#L2238 python3.pkgs.callPackage -> callPackage
+  # already implemented in https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/all-packages.nix#L2232
+  nixpkgs.overlays = [
+    (self: super: {
+      cloud-init = (
+        super.callPackage (super.path + "/pkgs/tools/virtualization/cloud-init") {
+          systemd = super.systemd;
+        }
+      );
+    })
+  ];
+  # FIXME: TEMP
+
   networking.useDHCP = lib.mkForce false;
 
   services.cloud-init = {
